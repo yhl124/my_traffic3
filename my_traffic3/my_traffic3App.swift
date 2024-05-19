8f4e3fabe4aebc8ea5f81d4bd091c3b90ef65d14
yh <yhl124.py@gmail.com>
2024-05-19 15:21:24 +0900

    Initial Commit

---
+++ my_traffic3/my_traffic3App.swift
@@ -0,0 +1,20 @@
+//
+//  my_traffic3App.swift
+//  my_traffic3
+//
+//  Created by yhl on 5/19/24.
+//
+
+import SwiftUI
+
+@main
+struct my_traffic3App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
